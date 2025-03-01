d6f19a2b9fcf1d8b0b5e9ab02989c415864a27a6
engabhishek <abhisheksharma879150@gmail.com>
2025-03-02 04:09:26 +0530

    create Button component

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,43 +1,49 @@
 import React, { useState, useContext } from "react";
 import InputField from "./general/InputField";
-import Header from "./general/Header.jsx"
+import Header from "./general/Header.jsx";
 import Title from "./general/Title";
 import Option from "./general/Option";
 import { AuthContext } from "../context/AuthProvider.jsx";
+import Button from "./general/Button.jsx";
 
 const LoginPage = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const {setUserData} = useContext(AuthContext); 
-       
-    return (
-        <>
-            <Header />
-            <Title
-                title={"Log-in to your account"}
-                subHeading={"Welcome back! Please enter your details."}
-            />
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { setUserData } = useContext(AuthContext);
 
-            <InputField
-                useStateVariable={email}
-                useStateFunction={setEmail}
-                label={"Email"}
-                type={"text"}
-                placeholder={"Enter your email or user id"}
-                imgSrc={"./src/assets/mail.png"}
-            />
+  return (
+    <>
+      <Header />
+      <Title
+        title={"Log-in to your account"}
+        subHeading={"Welcome back! Please enter your details."}
+      />
 
-            <InputField
-                useStateVariable={password}
-                useStateFunction={setPassword}
-                label={"Password"}
-                type={"password"}
-                placeholder={"Enter your password"}
-                imgSrc={"./src/assets/lock.png"}
-            />
-            <Option p1={"Change Password"} p2={"Forgot Password"} />
-        </>
-    );
+      <InputField
+        useStateVariable={email}
+        useStateFunction={setEmail}
+        label={"Email"}
+        type={"text"}
+        placeholder={"Enter your email or user id"}
+        imgSrc={"./src/assets/mail.png"}
+      />
+
+      <InputField
+        useStateVariable={password}
+        useStateFunction={setPassword}
+        label={"Password"}
+        type={"password"}
+        placeholder={"Enter your password"}
+        imgSrc={"./src/assets/lock.png"}
+      />
+      <Option 
+      p1={"Change Password"} 
+      p2={"Forgot Password"} 
+      />
+
+      <Button text="Log In" />
+    </>
+  );
 };
 
 export default LoginPage;
