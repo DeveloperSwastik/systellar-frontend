b24129c9fd18ece07ae253707dca6c4c77b37d50
DeveloperSwastik <101865485+DeveloperSwastik@users.noreply.github.com>
2025-03-02 00:53:49 +0530

    Add use state to InputField component

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import InputField from "./components/InputField";
 import Title from "./components/Title";
 
 function App() {
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
   return (
     <>
@@ -14,13 +14,19 @@ function App() {
       <Title title={"Log-in to your account"} subHeading={"Welcome back! Please enter your details."}/>
 
       <InputField
+        useStateVariable={email}
+        useStateFunction={setEmail}
         label={"Email"}
+        type={"text"}
         placeholder={"Enter your email or user id"}
         imgSrc={"./src/assets/mail.png"}
-      />
+        />
 
       <InputField
+        useStateVariable={password}
+        useStateFunction={setPassword}
         label={"Password"}
+        type={"password"}
         placeholder={"Enter your password"}
         imgSrc={"./src/assets/lock.png"}
       />
