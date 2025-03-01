5bf2a9760381332e9a4efb28af79df913bcfd378
engabhishek <abhisheksharma879150@gmail.com>
2025-03-02 00:50:45 +0530

    create Option component

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const InputField = (data) => {		
   return (
     <>
-      <div>
+      <div className="pt-4">
         <label className="font-semibold text-lg" htmlFor={data.label}>{data.label}</label>
         <div className="input bg-[#434141] rounded-xl p-1 mt-3 flex">
           <img className="w-8 opacity-50 mx-2" src={data.imgSrc} alt="" />
