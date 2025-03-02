dfd54dcdb896c395bceabb995c3ec48a28dc98b4
DeveloperSwastik <101865485+DeveloperSwastik@users.noreply.github.com>
2025-03-02 23:57:15 +0530

    Update FirmwareUpdate

--- src/components/FirmwareUpdate.jsx
+++ src/components/FirmwareUpdate.jsx
@@ -4,28 +4,29 @@ import Title from "./general/Title";
 import Button from "./general/Button";
 
 function FirmwareUpdate() {
-  return (
-    <>
-      <Header />
-      <Title title={"Firmware Update"} />
+    return (
+        <>
+            <Header />
+            <Title title={"Firmware Update"} />
 
-      <div className="bg-[url('./src/assets/FirmwareBackGround.png')] bg-cover bg-center w-full h-80 mt-7 rounded-xl ">
-      
-        <div className="">
-          <p className="px-4">Update available</p>
-          <h2 className="px-4 font-bold text-lg ">Charge Controller</h2>
-          <p className="px-4">zxcv765a</p>
-        </div>
-      </div>
+            <div className="sticky bottom-0 bg-[url('./src/assets/FirmwareBackGround.png')] bg-cover bg-center w-full h-80 mt-7 rounded-xl ">
+                <div className="">
+                    <p className="px-4 text-lg">Update available</p>
+                    <h2 className="px-4 font-bold text-2xl ">
+                        Charge Controller
+                    </h2>
+                    <p className="px-4 text-lg">zxcv765a</p>
+                </div>
+            </div>
 
-      <div className="bg-white pt-3 mt-7 pb-3 rounded-xl flex flex-col justify-center">
-        <h2 className="px-4 font-bold text-lg ">Firmware Version</h2>
-        <p className="px-5 ">vp123(1456ev)</p>
-      </div>
+            <div className="bg-white py-5 mt-7 rounded-xl flex flex-col justify-center">
+                <h2 className="px-4 font-bold text-lg ">Firmware Version</h2>
+                <p className="px-5 ">vp123(1456ev)</p>
+            </div>
 
-      <Button text="Download and Install" />
-    </>
-  );
+            <Button text="Download and Install" />
+        </>
+    );
 }
 
 export default FirmwareUpdate;
--- src/components/general/Button.jsx
+++ src/components/general/Button.jsx
@@ -3,10 +3,8 @@ import React from "react";
 function Button(data) {
   return (
     <>
-      <div className=" w-full flex pt-4 justify-center " >
-        <div className=" w-90 p-1 bg-[#fd7d06] text-white text-2xl font-semibold rounded-lg">
-          <button className="w-full ">{data.text}</button>
-        </div>
+      <div className=" w-full mt-7 p-1 justify-center bg-[#fd7d06] text-white text-2xl font-semibold rounded-lg" >
+          <button className="w-full">{data.text}</button>
       </div>
     </>
   );
